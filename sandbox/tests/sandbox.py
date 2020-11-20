618b41b81d1ce86786248cde56f8cf47c5b39434
Christopher Chung <tps90067@gmail.com>
2020-11-20 16:26:40 -0500

    Created new function and unit test

---
+++ sandbox/tests/sandbox.py
@@ -0,0 +1,29 @@
+def add_one(val):
+    """
+    Add one to a number.
+
+    Parameters
+    ----------
+    val: number
+        The number to which one should be added.
+
+    Returns
+    -------
+    The number plus one.
+    """
+    return val + 1
+
+def add_two(val):
+    """
+    Adds 2 to a number.
+
+    Parameters
+    ----------
+    val: number
+        The number to which one should be added.
+
+    Returns
+    -------
+    The number plus two.
+    """
+    return val + 2
--- sandbox/tests/test_sandbox.py
+++ sandbox/tests/test_sandbox.py
@@ -4,3 +4,7 @@ import sandbox as sb
 def test_add_one():
     val = 10
     assert sb.add_one(val) == 11
+
+def test_add_two():
+    val = 1
+    assert sb.add_two(val) == 3
