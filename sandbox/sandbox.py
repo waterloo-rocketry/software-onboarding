bb51c4ea65cafbed1e423aea8c345188150d0f39
Austin W. Milne <awbmilne@gmail.com>
2021-05-15 00:13:46 -0400

    Add fibonnaci_by_index function
    Add function for getting fibonnaci value by index.
    Add the requisite unit test for the function.
    
    Onboarding task for Austin W. Milne

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -13,6 +13,7 @@ def add_one(val):
     """
     return val + 1
 
+
 def add_two(val):
     """
     Add 2 to a number.
@@ -27,3 +28,33 @@ def add_two(val):
     The number plus two.
     """
     return val + 2
+
+
+def fibonacci_by_index(index: int):
+    """
+    Get the value at the index of the fibonacci series.
+
+    Austin W. Milne @awbmilne  <austin.milne@uwaterloo.ca>
+
+    Parameters
+    ----------
+    index: number
+        The index of the fibonacci series to return.
+
+    Returns
+    -------
+    The value from the fibonnacci series at the given index.
+    """
+    if (index < 0):
+        raise IndexError("Invalid Fibonacci Index")
+        return -1
+    if (index == 0):
+        return 0
+    else:
+        im2 = 0  # Value of at index i-2
+        im1 = 1  # Value of at index i-1
+        for count in range(index-1):
+            i = im2 + im1  # Sum the previous 2 numbers
+            im2 = im1     # Move i-1 to i-2
+            im1 = i       # Move i to i-1
+        return im1  # Return the latest i (now in i-1)
--- sandbox/tests/test_sandbox.py
+++ sandbox/tests/test_sandbox.py
@@ -5,6 +5,14 @@ def test_add_one():
     val = 10
     assert sb.add_one(val) == 11
 
+
 def test_add_two():
     val = 1
     assert sb.add_two(val) == 3
+
+
+def test_fibonnaci_by_index():
+    indexs = (0, 1, 2, 3, 19, 35, 74, 100)
+    values = (0, 1, 1, 2, 4181, 9227465, 1304969544928657, 354224848179261915075)
+    for index, val in zip(indexs, values):
+        assert sb.fibonacci_by_index(index) == val
