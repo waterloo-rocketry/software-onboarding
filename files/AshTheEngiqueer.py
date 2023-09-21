21c42235aa0fb3c3539e49e4c14ab4234a18c51f
Ash Lang <a7lang@uwaterloo.ca>
2023-09-21 19:31:26 -0400

    FOR TEST PURPOSES ONLY

---
+++ files/AshTheEngiqueer.py
@@ -0,0 +1,9 @@
+# Sample Python file
+
+
+def say_hello(name: str):
+	print(f"That's what they used to call me... {name}.")
+
+if __name__ == "__main__":
+	name = "Waterloo Rocketry"
+	say_hello(name)
