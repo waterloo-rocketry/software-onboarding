58f2e05e8de63eb1fff86f09a46bea3ec3ccf79d
Ozayr Raazi <67076803+Darth-Raazi@users.noreply.github.com>
2023-09-21 08:07:03 -0400

    Updated + Simplified for Fall 2023 (#27)
    * Simplified tutorial and updated readme
    
    * Updated Part 1
    
    * Formatting changes
    
    * Removed outdated info
    
    * Removed unit testing functionality
    
    * Added instructions to clone a repo
    
    * Changed file with functions to folder with files

---
+++ files/sample.py
@@ -0,0 +1,11 @@
+# Sample Python file
+# DO NOT MAKE CHANGES DIRECTLY TO THIS FILE
+# Instead, copy it and rename it to <your-GH-username>.py and make changes to that one
+
+
+def say_hello(name: str):
+	print(f"Hello, my name is {name}!")
+
+if __name__ == "__main__":
+	name = "Waterloo Rocketry"
+	say_hello(name)
