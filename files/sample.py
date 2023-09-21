ea549a64328f55e1cdb59c5ed1b7d3584bcd76a4
sherylchung <sherylchungtc@gmail.com>
2023-09-21 19:53:49 -0400

    my name

--- files/sample.py
+++ files/sample.py
@@ -3,6 +3,8 @@
 # Instead, copy it and rename it to <your-GH-username>.py and make changes to that one
 
 
+name = "Sheryl Chung”
+
 def say_hello(name: str):
 	print(f"Hello, my name is {name}!")
 
